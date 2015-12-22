769b4502807fbe999f71b1e8a89c4654fa2f3382
Jesse Kinkead <jesse.kinkead@gmail.com>
2015-12-22 14:11:49 -0800

    Some more paths implementation, plus some unit tests.

--- src/main/scala/pgb/BuildPaths.scala
+++ src/main/scala/pgb/BuildPaths.scala
@@ -1,25 +1,73 @@
 package pgb
 
+import java.io.File
 import java.net.{ URI, URISyntaxException }
+import java.nio.file.Files
 
 /** Class to handle manipulating pgb paths.
   *
   * Paths are URI-based; they have a scheme, usually `file:`, and are either relative or absolute.
   *
-  * This class has utility methods for turning task names into path URIs.
+  * This class has utility methods for turning task names into path URIs, resolving paths into
+  * files, and handling glob and lookbehind expansion.
+  *
+  * TODO: Document paths!
   */
 object BuildPaths {
-  /** Resolves the given path, aginst the given root if it is a relative path. This doesn't validate
-    * any aspects of the path itself, only the URI formatting.
+  /** Regex to match a path with a glob. The match starts after the last path separator. */
+  val FileGlob = """(?:^|/)[^/]*?(?<=[^\\])[*{\[?]""".r.unanchored
+
+  /** Given a path and a build root, returns the file(s) this path points to.
     * @param path the path to resolve, either relative or absolute
     * @param buildRoot the root to resolve the path against, if it is a relative path
     * @return the absolute, canonical version of the given path, resolved against the given build
     *     root if it was a relative path
+    * @throws IllegalArgumentException if the build root is a relative path
+    * @throws ConfigException if the provided path is invalid
+    */
+  def resolvePath(path: String, buildRoot: URI): Seq[File] = {
+    require(buildRoot.isAbsolute, s"""Build root "$buildRoot" was not absolute""")
+
+    // First, figure out if this is a relative path or an absolute path so that we can figure out
+    // the URI scheme.
+
+    // The URI version of the literal path provided. May be relative or absolute.
+    val pathUri = toPathUri(path)
+    val scheme = if (pathUri.isAbsolute) { pathUri.getScheme } else { buildRoot.getScheme }
+
+    // TODO: Factor this into a class.
+    val isFilesystem = scheme match {
+      case "file" | "github" => true
+      case "task" => false
+      case _ => throw new ConfigException(s"Unhandled scheme: $scheme")
+    }
+
+    if (path.startsWith(".../")) {
+      if (!isFilesystem) {
+        throw new ConfigException(
+          s"""Lookbehind path "$path" specified for non-filesystem scheme "$scheme""""
+        )
+      }
+      // TODO: Loop until we find files.
+      null
+    } else {
+      // TODO: Map build root to a directory, if filesystem. Else, resolve full path.
+      if (isFilesystem) {
+        // TODO: Map the build file to a base directory.
+        val baseDirectory: File = null
+      } else {
+      }
+      null
+    }
+  }
+
+  /** Converts a string into a path URI, validating the basic URI formatting.
+    * @param path the path to convert, either relative or absolute
+    * @return the canonical version of the given path
     * @throws ConfigException if the URI has invalid syntax or if it's a relative URI with a scheme
     *     set
     */
-  def resolvePath(path: String, buildRoot: URI): URI = {
-    require(buildRoot.isAbsolute, s"""Build root "$buildRoot" was not absolute""")
+  def toPathUri(path: String): URI = {
     val pathUri = try {
       new URI(path)
     } catch {
@@ -32,6 +80,29 @@ object BuildPaths {
       throw new ConfigException(s"""Non-absolute URI with scheme in "$path"""")
     }
 
-    buildRoot.resolve(pathUri).normalize
+    pathUri.normalize
+  }
+
+  /** Splits a path into its base component and any lookbehind or wildcard components. For paths
+    * with no special components, this will return the path unchanged, and an empty relative path.
+    * For paths with special components, the first part will contain the absolute URI of the path
+    * leading up to the first special component, and the second part will be a relative URI
+    * containing the special components.
+    *
+    * This assumes that the given path is absolute and non-opaque.
+    * @param path the URI of the path to split
+    * @return a pair of URIs, the first being the absolute base path, and the second being the
+    *     relative wildcard off of that
+    * @throws ConfigException if the given path has more than one lookbehind component, or if it has
+    *     wildcards preceeding the lookbehind component
+    */
+  def splitPath(path: URI): (URI, Option[URI]) = {
+    // This is everything after the authority (which might be the first path component).
+    val pathComponent = path.getRawPath
+    val lookbehindIndex = pathComponent.indexOf("/...")
+    // Ends with "/..." or contains "/.../".
+    if (lookbehindIndex == pathComponent.length - 4 || pathComponent(lookbehindIndex + 4) == '/') {
+    }
+    null
   }
 }
--- src/test/scala/pgb/BuildPathsSpec.scala
+++ src/test/scala/pgb/BuildPathsSpec.scala
@@ -5,6 +5,7 @@ import java.net.URI
 class BuildPathsSpec extends UnitSpec {
   val buildRoot = new URI("file:///home/jkinkead/build.pgb")
 
+  /*
   "resolvePath" should "resolve a relative file path" in {
     val result = BuildPaths.resolvePath("src/foo.scala", buildRoot)
     result shouldBe new URI("file:///home/jkinkead/src/foo.scala")
@@ -15,46 +16,9 @@ class BuildPathsSpec extends UnitSpec {
     result shouldBe new URI("file:///home/jkinkead/.../src/**/foo*.scala")
   }
 
-  it should "fail on opaque paths" in {
-    val exception = intercept[ConfigException] {
-      BuildPaths.resolvePath("file:src/foo.scala", buildRoot)
-    }
-    exception.getMessage should startWith("Non-absolute URI")
-  }
-
-  it should "resolve an absolute file path" in {
-    val path = "file:///home/jkinkead/other/src/foo.scala"
-    val result = BuildPaths.resolvePath(path, buildRoot)
-    // Result should be an unmodified path.
-    result shouldBe new URI(path)
-  }
-
-  it should "resolve an absolute github path" in {
-    val path = "github:///jkinkead/project/foo.pgb#task"
-    val result = BuildPaths.resolvePath(path, buildRoot)
-    // Result should be an unmodified path.
-    result shouldBe new URI(path)
-  }
-
-  it should "canonicalize an absolute path" in {
-    val result =
-      BuildPaths.resolvePath("file:///jkinkead/other/src/main/../test/foo.scala", buildRoot)
-    // Result should be an unmodified path.
-    result shouldBe new URI("file:///jkinkead/other/src/test/foo.scala")
-  }
-
-  it should "canonicalize a relative path" in {
-    val result = BuildPaths.resolvePath("test/../main/foo.scala", buildRoot)
-    // Result should be an unmodified path.
-    result shouldBe new URI("file:///home/jkinkead/main/foo.scala")
-  }
-
-  it should "throw an exception for a badly-formatted URI" in {
-    val exception = intercept[ConfigException] {
-      // Badly-formatted URI encoding.
-      BuildPaths.resolvePath("foo%2scala", buildRoot)
-    }
-    exception.getMessage should startWith("Bad URI in path")
+  it should "resolve paths with only a fragment" in {
+    val result = BuildPaths.resolvePath("#task", buildRoot)
+    result shouldBe new URI("file:///home/jkinkead/build.pgb#task")
   }
 
   it should "throw an exception if given a relative buildRoot" in {
@@ -63,5 +27,74 @@ class BuildPathsSpec extends UnitSpec {
       BuildPaths.resolvePath("foo.scala", new URI("jkinkead/project"))
     }
     exception.getMessage should include("Build root")
+  }
+  */
+
+  "toPathUri" should "fail on opaque paths" in {
+    val exception = intercept[ConfigException] {
+      BuildPaths.toPathUri("file:src/foo.scala")
+    }
+    exception.getMessage should startWith("Non-absolute URI")
+  }
+
+  it should "work on an absolute file path" in {
+    val path = "file:///home/jkinkead/other/src/foo.scala"
+    val result = BuildPaths.toPathUri(path)
+    // Result should be an unmodified path.
+    result shouldBe new URI(path)
+  }
+
+  it should "work on an absolute github path" in {
+    val path = "github:///jkinkead/project/foo.pgb#task"
+    val result = BuildPaths.toPathUri(path)
+    // Result should be an unmodified path.
+    result shouldBe new URI(path)
+  }
+
+  it should "canonicalize an absolute path" in {
+    val result = BuildPaths.toPathUri("file:///jkinkead/other/src/main/../test/foo.scala")
+    // Result should be a canonicalized path.
+    result shouldBe new URI("file:///jkinkead/other/src/test/foo.scala")
+  }
+
+  it should "canonicalize a relative path" in {
+    val result = BuildPaths.toPathUri("test/../main/foo.scala")
+    // Result should be a canonicalized path.
+    result shouldBe new URI("main/foo.scala")
+  }
+
+  it should "throw an exception for a badly-formatted URI" in {
+    val exception = intercept[ConfigException] {
+      // Badly-formatted URI encoding.
+      BuildPaths.toPathUri("foo%2scala")
+    }
+    exception.getMessage should startWith("Bad URI in path")
+  }
+
+  "FileGlob" should "match a basic path correctly" in {
+    val path = "src/main/scala/**/*.scala"
+    val matchOption = BuildPaths.FileGlob.findFirstMatchIn(path)
+    matchOption shouldBe 'nonEmpty
+    val m = matchOption.get
+    m.before shouldBe "src/main/scala"
+    path.substring(m.start) shouldBe "/**/*.scala"
+  }
+
+  it should "match a path starting with a glob" in {
+    val path = "**/*.java"
+    val matchOption = BuildPaths.FileGlob.findFirstMatchIn(path)
+    matchOption shouldBe 'nonEmpty
+    val m = matchOption.get
+    m.before shouldBe ""
+    path.substring(m.start) shouldBe "**/*.java"
+  }
+
+  it should "match an escaped path correctly" in {
+    val path = """src/ma\*in/scala/*.scala"""
+    val matchOption = BuildPaths.FileGlob.findFirstMatchIn(path)
+    matchOption shouldBe 'nonEmpty
+    val m = matchOption.get
+    m.before shouldBe """src/ma\*in/scala"""
+    path.substring(m.start) shouldBe "/*.scala"
   }
 }
